cb37ab43b5919f60add46ca4f6c675e206475ed5
Przemysław Walkiewicz <przemyslaw.walkiewicz@o2.pl>
2018-06-30 13:15:10 +0200

    informaje

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+8
+git1.iml,b\3\b3faeb77373f1acb1067acdcadef7de343677374
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/main/java/gitLekcja4.java
@@ -0,0 +1,6 @@
+public class gitLekcja4 {
+    public static void main (String[] args){
+        String zmienna = "Klonujemy";
+        System.out.println(zmienna/*gdy w tym miejscu dodam kropke to mi wyświetli podpowiedzi jakich funkcji moge użyć*/);
+    }
+}
